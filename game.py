67f4ac9829079845fd47e82b526b9308d8a44112
Theodor Ström <look4totte@gmail.com>
2019-03-05 23:48:49 +0100

    fixed minor spelling mistakes and stuff

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-#Every time a event gets succes or failed then its added into this dictionary so it can easely be accesed later in the game when we need to call them
-eventSucces = { "kanndyEvent1": True , "kanndyevent2": False }
-#the dic works like this the key is the name of the of the character + the event and the value if it succesed or not
+#Every time a event gets success or failed then its added into this dictionary so it can easily be accessed later in the game when we need to call them
+eventSuccess = { "kanndyEvent1": True , "kanndyevent2": False }
+#the dict works like this the key is the name of the of the character + the event and True/False whether it succeeded or not
 import eventHandler
 
 student = eventHandler.Student("kanndy")
 event = student.event1()
-eventSucces['eventTest'] = event
+eventSuccess['eventTest'] = event
 del student
-print(eventSucces['eventTest'])
+print(eventSuccess['eventTest'])
