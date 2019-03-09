fb99eb657824e30d246fd15d65397fb1c05dcc23
Theodor Ström <look4totte@gmail.com>
2019-03-09 13:08:57 +0100

    Changed up callEnd since python 3.5 doesn't have enumeration support, also changed execScript in a way that hopefully makes it less fragile

--- game.py
+++ game.py
@@ -1,9 +1,4 @@
 import os
-from enum import Enum
-
-class End(Enum):
-     GOOD = 1
-     BAD = 0
 
 #Every time a event gets success or failed then its added into this dictionary so it can easily be accessed later in the game when we need to call them
 #the dict works like this the key is the name of the of the character + the event and True/False whether it succeeded or not
@@ -11,6 +6,8 @@ eventSuccess = { "kanndyEvent1": True , "kanndyevent2": False }
 
 characterDirectory = "Character" # Name for the character directory
 
+GOOD_END = "good"
+BAD_END = "bad"
 
 def execScript(characterName, scriptName, extract=[]):
      """
@@ -20,16 +17,16 @@ def execScript(characterName, scriptName, extract=[]):
      scriptName: the script that we wish to execute
      extract: an array of variable names that we wish to extract out of the script execution, can be empty if we don't want anything
      """
-     _locals = locals() # get a copy of the local namespace
+     to_extract = {} #locals() # get a copy of the local namespace
      exec(open(
           # using os.path.join to make sure we get correct behaviour regardless of OS
           os.path.join(
                characterDirectory,
                characterName,
-               scriptName
+               scriptName + ".py"
           )
-     ).read(), globals(), _locals) # read in the file, then hand it the local namespace. No idea why we need to do this, and it's probably a bad idea, but it works
-     return [_locals[var] for var in extract] # return every local variable from script execution that was mentioned in extract
+     ).read(), globals(), to_extract) # read in the file, then hand it the local namespace. No idea why we need to do this, and it's probably a bad idea, but it works
+     return [to_extract[var] for var in extract] # return every local variable from script execution that was mentioned in extract
 
 
 def callEvent(characterName, number, cli=False):
@@ -40,21 +37,19 @@ def callEvent(characterName, number, cli=False):
      number: which number the event is (must be 1-6, inclusive)
      cli: whether the event is a climax or not (default: False)
      """
-     assert(1 <= number <= 6, "event number was outside the range [1, 6]")
-     scriptName = "event{}{}.py".format("Cli" if cli else "", number)
+     assert 1 <= number <= 6, "event number was outside the range [1, 6]"
+     scriptName = "event{}{}".format("Cli" if cli else "", number)
      eventClimax = execScript(characterName, scriptName, ["eventClimax"])[0]
      eventSuccess[characterName + scriptName] = eventClimax
 
-def callEnd(characterName, endtype):
+def callEnd(characterName, endType):
      """
      Call a specific character's ending
 
      characterName: the character's name
-     endtype: the type of ending (good/bad). Use the `End` enum for this.
+     endType: the type of ending (good/bad). Use the constants GOOD_END and BAD_END to specify endType please
      """
-     if endtype == End.GOOD:
-          execScript(characterName, "goodEnd.py")
-     elif endtype == End.BAD:
-          execScript(characterName, "badEnd.py")
+     execScript(characterName, "{}End".format(endType))
 
-print(eventSuccess['eventTest'])
+callEvent("kanndy", 1)
+print(eventSuccess['kanndyevent1'])
